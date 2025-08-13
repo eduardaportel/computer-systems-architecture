40850511e92039d6aae46c50cf6d51721fb56c3d
Eduarda Portel <eduarda.portel4@gmail.com>
2025-08-12 22:17:28 -0300

    1.2

--- script.js
+++ script.js
@@ -1,21 +1,39 @@
-// declarando constantes
-const nome = "Eduarda"
-const idade = 27
-// aspas simples e duplas com o mesmo efeito
-const sexo = "M"
-const endereco = "Rua das Flores, 123"
-const outro_end = "Rua K, 2"
-// declarando variáveis
-// let: variável tem o escopo de bloco
-{
-    let x = 0
-}
-let a = 2
-let b = "abcd"
-// var: escopo da função onde é criado ou global
-var c = 2 + 3
-var d = "abcd"
+var lang = "JavaScript"
+console.log("Learning " + lang)
 
-console.log(a)
-console.log(b)
-console.log(c,d)
+var lang = "Java"
+console.log("Learning " + lang)
+
+var age = 20
+console.log(`Hi, ${name}`)
+
+if (age >= 20){
+    var name = "Eduarda"
+    console.log(`Congrats, ${name}. You can drive now!`)
+}
+
+
+
+
+
+// // declarando constantes
+// const nome = "Eduarda"
+// const idade = 27
+// // aspas simples e duplas com o mesmo efeito
+// const sexo = "M"
+// const endereco = "Rua das Flores, 123"
+// const outro_end = "Rua K, 2"
+// // declarando variáveis
+// // let: variável tem o escopo de bloco
+// {
+//     let x = 0
+// }
+// let a = 2
+// let b = "abcd"
+// // var: escopo da função onde é criado ou global
+// var c = 2 + 3
+// var d = "abcd"
+
+// console.log(a)
+// console.log(b)
+// console.log(c,d)
