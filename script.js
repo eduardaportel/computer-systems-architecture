84d695ebc170c427a1ee214a376f79423eafac17
Eduarda Portel <eduarda.portel4@gmail.com>
2025-08-12 22:09:50 -0300

    1.1

---
+++ script.js
@@ -0,0 +1,21 @@
+// declarando constantes
+const nome = "Eduarda"
+const idade = 27
+// aspas simples e duplas com o mesmo efeito
+const sexo = "M"
+const endereco = "Rua das Flores, 123"
+const outro_end = "Rua K, 2"
+// declarando variáveis
+// let: variável tem o escopo de bloco
+{
+    let x = 0
+}
+let a = 2
+let b = "abcd"
+// var: escopo da função onde é criado ou global
+var c = 2 + 3
+var d = "abcd"
+
+console.log(a)
+console.log(b)
+console.log(c,d)
