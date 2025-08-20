d852b3e2a0ea2c2004ab92961c5da583f9f08d2e
Eduarda Portel <eduarda.portel4@gmail.com>
2025-08-19 21:29:03 -0300

    funções

--- script.js
+++ script.js
@@ -1,66 +1,122 @@
-// comparação
-console.log(1 == 1) // true
-console.log(1 == '1') // true
-console.log(1 === "1") // false 
-console.log(1 === 1) // true com tipos iguais
-console.log(true == 1)
-console.log(1 == [1]) 
-console.log(null === null)
-console.log(undefined === undefined)
+// var a = [1]
+// var b = [1]
 
+// console.log(a == b) // false, pois são objetos diferentes na memória
 
+// var c = a
 
+// console.log(a == c) // true
+// console.log([1] == [1]) // false
+// console.log(a === c) // true
 
+// const names = ['Eduarda', 'João', 'Maria']
+// const apenasComE = names.filter((n) => n.startsWith('E'))
+// console.log(apenasComE) // ['Eduarda']
 
-// // coerção
-// const n1 = 1
-// const n2 = '3'
-// // coerção implícita
-// console.log(n1 + n2)
-// // coerção explícita
-// const n4 = n1 + Number(n2)
-// console.log(n4)
+// variável que não muda, posso adicionar valores, mas não posso reatribuir
+// const v1 = []
 
+// v1[0] = 1
+// v1[1] = 2
 
+// console.log(v1)
 
+// // funções
 
-
-
-// var lang = "JavaScript"
-// console.log("Learning " + lang)
-
-// var lang = "Java"
-// console.log("Learning " + lang)
-
-// var age = 20
-// console.log(`Hi, ${name}`)
-
-// if (age >= 20){
-//     var name = "Eduarda"
-//     console.log(`Congrats, ${name}. You can drive now!`)
+// function hello(){
+//     console.log("Hello, World!")
 // }
 
+// hello()
+// hello('João')
 
-
-
-// // declarando constantes
-// const nome = "Eduarda"
-// const idade = 27
-// // aspas simples e duplas com o mesmo efeito
-// const sexo = "M"
-// const endereco = "Rua das Flores, 123"
-// const outro_end = "Rua K, 2"
-// // declarando variáveis
-// // let: variável tem o escopo de bloco
-// {
-//     let x = 0
+// function hello(nome){
+//     console.log(`Hello, ${nome}`)
 // }
-// let a = 2
-// let b = "abcd"
-// // var: escopo da função onde é criado ou global
-// var c = 2 + 3
-// var d = "abcd"
 
-// console.log(a)
-// console.log(b)
-// console.log(c,d)
+// hello('João')
+
+// function soma(a,b){
+//     return a + b
+// }
+
+// res = soma(2,3)
+
+// console.log(res)
+// console.log(soma(2,3))
+
+
+
+
+
+
+
+
+
+
+// // // comparação
+// // console.log(1 == 1) // true
+// // console.log(1 == '1') // true
+// // console.log(1 === "1") // false 
+// // console.log(1 === 1) // true com tipos iguais
+// // console.log(true == 1)
+// // console.log(1 == [1]) 
+// // console.log(null === null)
+// // console.log([] = [])
+
+
+
+
+
+// // // coerção
+// // const n1 = 1
+// // const n2 = '3'
+// // // coerção implícita
+// // console.log(n1 + n2)
+// // // coerção explícita
+// // const n4 = n1 + Number(n2)
+// // console.log(n4)
+
+
+
+
+
+
+// // var lang = "JavaScript"
+// // console.log("Learning " + lang)
+
+// // var lang = "Java"
+// // console.log("Learning " + lang)
+
+// // var age = 20
+// // console.log(`Hi, ${name}`)
+
+// // if (age >= 20){
+// //     var name = "Eduarda"
+// //     console.log(`Congrats, ${name}. You can drive now!`)
+// // }
+
+
+
+
+// // // declarando constantes
+// // const nome = "Eduarda"
+// // const idade = 27
+// // // aspas simples e duplas com o mesmo efeito
+// // const sexo = "M"
+// // const endereco = "Rua das Flores, 123"
+// // const outro_end = "Rua K, 2"
+// // // declarando variáveis
+// // // let: variável tem o escopo de bloco
+// // {
+// //     let x = 0
+// // }
+// // let a = 2
+// // let b = "abcd"
+// // // var: escopo da função onde é criado ou global
+// // var c = 2 + 3
+// // var d = "abcd"
+
+// // console.log(a)
+// // console.log(b)
+// // console.log(c,d)
